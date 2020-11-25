983b279a9c7eeeed2850dea84097cd3949dccd26
izaanjahangir <izaanjahangir2@gmail.com>
2020-11-26 01:58:42 +0500

    added notification service to read whatsapp notifications

--- app/src/main/java/com/izaan/instagramclone/MainActivity.java
+++ app/src/main/java/com/izaan/instagramclone/MainActivity.java
@@ -2,8 +2,11 @@ package com.izaan.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        startActivity(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS));
     }
 
     public void handleLoginPress(View view){
@@ -23,4 +28,22 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SignupActivity.class);
         startActivity(intent);
     }
+
+//    private boolean isNotificationServiceEnabled(){
+//        String pkgName = getPackageName();
+//        final String flat = Settings.Secure.getString(getContentResolver(),
+//                "enabled_notification_listeners");
+//        if (!TextUtils.isEmpty(flat)) {
+//            final String[] names = flat.split(":");
+//            for (String name: names) {
+//                final ComponentName cn = ComponentName.unflattenFromString(name);
+//                if (cn != null) {
+//                    if (TextUtils.equals(pkgName, cn.getPackageName())) {
+//                        return true;
+//                    }
+//                }
+//            }
+//        }
+//        return false;
+//    }
 }
