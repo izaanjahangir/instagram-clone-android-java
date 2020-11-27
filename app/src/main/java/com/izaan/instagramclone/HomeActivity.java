8f1b958c16a1bce898e2b19869407e2aec42529a
izaanjahangir <izaanjahangir2@gmail.com>
2020-11-28 02:53:15 +0500

    added basic bottom tab bar

--- app/src/main/java/com/izaan/instagramclone/HomeActivity.java
+++ app/src/main/java/com/izaan/instagramclone/HomeActivity.java
@@ -1,8 +1,16 @@
 package com.izaan.instagramclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +18,39 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNav = findViewById(R.id.homeBottomNav);
+        bottomNav.setOnNavigationItemSelectedListener(handleNavigationListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.innerConstraintLayout, new ExploreFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener handleNavigationListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragmentItem = null;
+
+            int itemId = item.getItemId();
+            Log.i("izaan", "testing...");
+            if(itemId == R.id.exploreNavItem) {
+                fragmentItem = new ExploreFragment();
+            }
+
+            if(itemId == R.id.profileNavItem) {
+                fragmentItem = new ProfileFragment();
+            }
+
+            if(itemId == R.id.uploadNavItem) {
+                fragmentItem = new UploadFragment();
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.innerConstraintLayout, fragmentItem).commit();
+
+            return false;
+        }
+    };
+
+    public void handleUploadTabClick(View view){
+//        Toast.makeText(getApplicationContext(),"All fields are required",Toast.LENGTH_LONG).show();
     }
 }
